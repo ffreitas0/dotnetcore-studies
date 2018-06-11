2b8f315d791272f515ffa880f24a66c8fc85786c
Fabio Freitas <ffreitas0@gmail.com>
2018-06-11 10:58:58 -0300

    Tutorial

---
+++ ToDo-API/Todo.Model/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Todo.Model
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
