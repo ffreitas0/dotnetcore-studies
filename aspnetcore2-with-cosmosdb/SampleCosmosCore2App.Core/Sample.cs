ff16bfd89ec1c3b01f33cc70366a4a0a602cca11
Fabio Freitas <ffreitas0@gmail.com>
2018-09-11 21:15:22 -0300

    adding project aspnetcore2-with-cosmosdb to learn how to use cosmosdb

---
+++ aspnetcore2-with-cosmosdb/SampleCosmosCore2App.Core/Sample.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SampleCosmosCore2App.Core
+{
+    public class Sample
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        public string Content { get; set; }
+    }
+}
